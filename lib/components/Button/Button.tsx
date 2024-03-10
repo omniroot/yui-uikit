d2d568a1d489eae1e21344c10bae5d13ccf6349c
gamenarkyt <gagaritin11@gmail.com>
2024-03-10 22:33:51 +0300

    add base button component

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -1,15 +1,39 @@
-import {  DetailedHTMLProps, FC, HTMLAttributes } from "react";
-import styles from './Button.module.css'
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC, HTMLAttributes } from "react";
+import styles from "./Button.module.css";
 import clsx from "clsx";
+import { Loader } from "lib/components/Loader/Loader";
 
-interface ButtonProps extends DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    variant: "primary" | "outline"
+interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  variant?: "primary" | "secondary" |"outline";
+  isLoading?: boolean;
 }
-export const Button: FC<ButtonProps> = ({ variant, className, children,...rest }) => {
-    const _class = clsx(styles.defaultButton, className, {
-      [styles.primaryButton]: variant === "primary",
-      [styles.outlineButton]: variant === "outline"
-    })
-    
-  return (<button {...rest} className={_class}>{children}</button>);
+export const Button: FC<ButtonProps> = ({
+  variant = "primary",
+  isLoading = false,
+  disabled = false,
+  className,
+  children,
+  ...rest
+}) => {
+  const _class = clsx(styles.defaultButton, className, {
+    [styles.primaryButton]: variant === "primary",
+    [styles.secondaryButton]: variant === "secondary",
+    [styles.outlineButton]: variant === "outline",
+    [styles.isLoadingButton]: isLoading === true,
+    [styles.disabledButton]: disabled === true
+  });
+
+  return (
+    <button {...rest} className={_class} disabled={disabled}>
+      {isLoading && <Loader variant="small"/>}
+      <span style={isLoading ? {opacity: 0.8} : {opacity: 1}}>
+
+      {children}
+      </span>
+    </button>
+  );
 };
