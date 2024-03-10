254cc029b4e253cf472cbd7ef2425e49d3f2d253
gamenarkyt <gagaritin11@gmail.com>
2024-03-10 22:32:26 +0300

    add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from 'vite-plugin-dts'
 import {libInjectCss} from 'vite-plugin-lib-inject-css'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,8 @@ export default defineConfig({
   }},
   plugins: [react(),
     dts({ include: ['lib'] }),
-    libInjectCss()
+    libInjectCss(),
+    svgr()
   ],
   build: {
     lib: {
