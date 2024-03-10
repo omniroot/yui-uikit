0fcdfaa8b5b2c0eb2858a9667193eac0e40ad246
gamenarkyt <gagaritin11@gmail.com>
2024-03-10 22:34:56 +0300

    add preview page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { Button } from "lib"
+import { Button } from "lib";
+import styles from "./App.module.css";
+import { Loader } from "lib/components/Loader/Loader";
 
 function App() {
-
   return (
-    <div>
+    <div className={styles.App}>
       <span>Preview:</span>
-      <Button variant="primary">123</Button>
-      <Button variant="outline">123</Button>
+      <Button variant="primary">primary</Button>
+      <Button variant="primary" isLoading>primary</Button>
+      <Button variant="primary" disabled>primary</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="secondary" isLoading>secondary</Button>
+      <Button variant="secondary" disabled>secondary</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="outline" isLoading>outline</Button>
+      <Button variant="outline" disabled>outline</Button>
+
+      <Loader variant="small" />
+      <Loader variant="standart" />
+      <Loader variant="fullscreen" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
