67b041827c71f686e704f9bfdd964391e162de3d
gamenarkyt <gagaritin11@gmail.com>
2024-03-11 23:39:59 +0300

    done base input component

--- lib/components/Input/Input.tsx
+++ lib/components/Input/Input.tsx
@@ -1,13 +1,71 @@
 import clsx from "clsx";
-import { DetailedHTMLProps, FC, InputHTMLAttributes } from "react";
+import {
+  ChangeEvent,
+  DetailedHTMLProps,
+  FC,
+  FormEvent,
+  InputHTMLAttributes,
+  useRef,
+  useState,
+} from "react";
 import styles from "./Input.module.css";
 
 interface InputProps
   extends DetailedHTMLProps<
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
-  > {}
-export const Input: FC<InputProps> = ({}) => {
+  > {
+  onChangeCallback?: (text: string) => void;
+  onSubmitCallback?: (text: string) => void;
+  symbolCount?: boolean;
+}
+export const Input: FC<InputProps> = ({
+  onChangeCallback,
+  onSubmitCallback,
+  symbolCount = true,
+  placeholder = "Placeholder",
+  ...rest
+}) => {
+  const [value, setValue] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
   const _class = clsx(styles.defaultInput);
-  return <input type="text" className={_class} />;
+
+  const onInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const _text = event.target.value;
+    if (onChangeCallback) {
+      onChangeCallback(_text);
+    }
+    setValue(_text);
+  };
+
+  const onFormClickHandler = () => {
+    inputRef.current?.focus();
+  };
+
+  const onSubmitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (onSubmitCallback) {
+      onSubmitCallback(value);
+    }
+  };
+
+  return (
+    <form
+      className={styles.defaultForm}
+      onClick={onFormClickHandler}
+      onSubmit={onSubmitHandler}
+    >
+      <input
+        value={value}
+        ref={inputRef}
+        onChange={onInputChangeHandler}
+        placeholder={placeholder}
+        className={_class}
+        {...rest}
+      />
+      {symbolCount && (
+        <span className={styles.symbolCount}>{value.length} {rest.maxLength && `/ ${rest.maxLength}`}</span>
+      )}
+    </form>
+  );
 };
