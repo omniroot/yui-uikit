6a4d139cd342b4e711a746396d8fddd04dc37349
gamenarkyt <gagaritin11@gmail.com>
2024-05-29 21:22:45 +0300

    rewrite Popup component

--- lib/components/Popup/Popup.tsx
+++ lib/components/Popup/Popup.tsx
@@ -1,44 +1,77 @@
-import { DetailedHTMLProps, FC, useState } from "react"
-import styles from "./Popup.module.css"
-import clsx from "clsx"
+import React, { FC, useState } from "react";
+import styles from "./Popup.module.css";
+import clsx from "clsx";
+import { BaseComponent } from "lib/types/types";
+import { Button } from "../Button/Button";
 
-interface PopupProps
-	extends DetailedHTMLProps<
-		React.HTMLAttributes<HTMLDivElement>,
-		HTMLDivElement
-	> {
-	listItems?: React.ReactNode[]
-	position?: "left" | "right"
+interface PopupListItem {
+  id?: string;
+  icon?: React.ReactNode;
+  title?: string;
+  onClick?: () => void;
+}
+
+interface PopupProps extends BaseComponent {
+  listItems?: PopupListItem[];
+  position?: "left" | "right";
+  width?: string;
+  height?: string;
 }
 
 export const Popup: FC<PopupProps> = ({
-	listItems,
-	position = "right",
-	className,
-	children,
+  listItems = [],
+  position = "right",
+  className,
+  width = "max-content",
+  height = "max-content",
+  children,
 }) => {
-	const [isOpen, setIsOpen] = useState(false)
-	const _class = clsx(styles.defaultPopupElements, className, {
-		[styles.positionLeft]: position === "left",
-		[styles.positionRight]: position === "right",
-	})
+  const [isOpen, setIsOpen] = useState(false);
+  const _class = clsx(styles.defaultPopupElements, className, {
+    [styles.positionLeft]: position === "left",
+    [styles.positionRight]: position === "right",
+  });
 
-	const onPopupButtonClick = () => {
-		setIsOpen((prev) => !prev)
-	}
+  const onPopupButtonClick = () => {
+    setIsOpen((prev) => !prev);
+  };
 
-	return (
-		<div className={styles.popupContainer}>
-			<div onClick={onPopupButtonClick} className={styles.popupButton}>
-				{children}
-			</div>
-			{isOpen && (
-				<div className={_class}>
-					{listItems?.map((ell) => {
-						return <div className={styles.popupElement}>{ell}</div>
-					})}
-				</div>
-			)}
-		</div>
-	)
-}
+  return (
+    <div className={styles.popupContainer}>
+      <Button
+        onClick={onPopupButtonClick}
+        variant="transparent"
+        className={styles.popupButton}
+      >
+        {children}
+      </Button>
+      {isOpen && (
+        <div
+          className={_class}
+          style={
+            position == "left"
+              ? {
+                  width: width,
+                  height: height,
+                  right: 0,
+                }
+              : { width: width, height: height, left: 0 }
+          }
+        >
+          {listItems.map((ell) => {
+            return (
+              <Button
+                className={styles.popupElement}
+                variant="transparent"
+                key={ell.id}
+                onClick={ell.onClick}
+              >
+                {ell.icon} {ell.title}
+              </Button>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
