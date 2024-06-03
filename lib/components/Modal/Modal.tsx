83b0dcb431f8b61de2acd87d80ca6bd4ea23fec3
gamenarkyt <gagaritin11@gmail.com>
2024-06-03 17:19:15 +0300

    refactoring modal

--- lib/components/Modal/Modal.tsx
+++ lib/components/Modal/Modal.tsx
@@ -7,26 +7,34 @@ interface ModalProps {
   titleSlot?: React.ReactNode;
   rightSlot?: React.ReactNode;
   children?: React.ReactNode;
-  position?: "top" | "center" | "bottom",
-  closeCallback?: () => void
+  position?: "top" | "center" | "bottom";
+  closeCallback?: () => void;
+  key?: string | number;
 }
 
-export const Modal: FC<ModalProps> = ({ titleSlot, rightSlot, closeCallback, position = "center", children }) => {
-    const _class = clsx(styles.modal, {
-        [styles.positionTop]: position === "top",
-        [styles.positionCenter]: position === "center",
-        [styles.positionBottom]: position === "bottom",
-    })
+export const Modal: FC<ModalProps> = ({
+  titleSlot,
+  rightSlot,
+  closeCallback,
+  position = "center",
+  key,
+  children,
+}) => {
+  const _class = clsx(styles.modal, {
+    [styles.positionTop]: position === "top",
+    [styles.positionCenter]: position === "center",
+    [styles.positionBottom]: position === "bottom",
+  });
 
-    const _closeModal = () => {
-        closeCallback && closeCallback()
-    }
+  const _closeModal = () => {
+    closeCallback && closeCallback();
+  };
 
-    const onModalClickHandler = (event : MouseEvent<HTMLDivElement>) => {
-        event.stopPropagation()
-    }
+  const onModalClickHandler = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+  };
   return (
-    <div className={styles.modalContainer} onClick={_closeModal}>
+    <div key={key} className={styles.modalContainer} onClick={_closeModal}>
       <div className={_class} onClick={onModalClickHandler}>
         <Typography.Heading rightSlot={rightSlot}>
           {titleSlot}
