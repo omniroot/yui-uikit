65ef99ff97493ee8f874e71e43b0a4d320278dc4
gamenarkyt <gagaritin11@gmail.com>
2024-06-03 17:20:05 +0300

    refactoring

--- lib/components/Typography/HeadingTypography/HeadingTypography.tsx
+++ lib/components/Typography/HeadingTypography/HeadingTypography.tsx
@@ -1,32 +1,32 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, FC, ReactNode } from "react"
-import typographyStyles from "../Typography.module.css"
-import styles from "./HeadingTypography.module.css"
-import clsx from "clsx"
-import { Typography } from "lib/components/Typography/Typography"
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC, ReactNode } from "react";
+import typographyStyles from "../Typography.module.css";
+import styles from "./HeadingTypography.module.css";
+import clsx from "clsx";
+import { Typography } from "lib/components/Typography/Typography";
 
 interface HeadingTypographyProps
-	extends DetailedHTMLProps<
-		ButtonHTMLAttributes<HTMLSpanElement>,
-		HTMLSpanElement
-	> {
-	rightSlot?: ReactNode
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  > {
+  rightSlot?: React.ReactNode;
 }
 
 export const HeadingTypography: FC<HeadingTypographyProps> = ({
-	children,
-	rightSlot,
-	...rest
+  children,
+  rightSlot,
+  ...rest
 }) => {
-	const _class = clsx(
-		styles.headingTypographyDefault,
-		typographyStyles.typographySize2
-	)
-	return (
-		<div className={styles.headingContainer}>
-			<Typography className={_class} {...rest}>
-				{children}
-			</Typography>
-			{rightSlot}
-		</div>
-	)
-}
+  const _class = clsx(
+    styles.headingTypographyDefault,
+    typographyStyles.typographySize2
+  );
+  return (
+    <div className={styles.headingContainer}>
+      <Typography className={_class} {...rest}>
+        {children}
+      </Typography>
+      {rightSlot}
+    </div>
+  );
+};
